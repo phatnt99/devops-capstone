a814d5ba0f46b40c5f11cfb92d1be449ed5c1712
phatnt99 <phatnguyen240899@gmail.com>
2022-06-05 16:04:05 +0700

    fix lint python

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask.logging import create_logger
 import logging
 from datetime import date
@@ -15,4 +15,4 @@ def entry():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
-    Log.info(f"App is starting at port {port}")
+    Log.info(f"App is starting at port 80")
